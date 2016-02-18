048aee47f4849ad5d244d2896fb461dc210bfa13
git <123456>
2016-02-18 10:33:56 +0800

    xiong

---
+++ smartpen/src/main/java/com/cleverm/smartpen/app/Test.java
@@ -0,0 +1,19 @@
+package com.cleverm.smartpen.app;
+
+/**
+ * Created by xiong,An android project Engineer,on 2016/2/18.
+ * Data:2016-02-18  10:33
+ * Base on clever-m.com(JAVA Service)
+ * Describe:
+ * Version:1.0
+ * Open source
+ */
+public class Test {
+
+
+    /**
+     *
+     */
+
+
+}
