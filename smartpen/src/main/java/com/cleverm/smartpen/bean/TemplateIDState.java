71a9f048f36caf14069e77de24b0bb159373009b
terry <terry.liu@clever-m.com>
2016-02-23 16:20:40 +0800

    add versionUpdata

---
+++ smartpen/src/main/java/com/cleverm/smartpen/bean/TemplateIDState.java
@@ -0,0 +1,25 @@
+package com.cleverm.smartpen.bean;
+
+/**
+ * Created by 95 on 2016/2/23.
+ */
+public class TemplateIDState {
+    private int id;
+    private boolean isSend;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public void setIsSend(boolean isSend) {
+        this.isSend = isSend;
+    }
+}
